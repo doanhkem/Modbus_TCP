3c9eadacdff6fde9838eade545e058a89c0dda22
doanhkem <doanhnguyen2.com@gmail.com>
2023-10-16 14:56:41 +0700

    odbus_v3

--- read.py
+++ read.py
@@ -8,9 +8,16 @@ import paho.mqtt.client as mqtt
 import threading
 import pickle
 
-timeset = 0
+
 data_package = {}
 DATA = {}
+timestart = time.time()
+irra = 0
+nhietdo = 0
+status = None
+delaysec = 60
+data_queue = []
+timeset = None
 with open('backup.pickle', 'rb') as f:
     backup = pickle.load(f)
 if backup != 0:
@@ -18,30 +25,50 @@ if backup != 0:
     print(total)
 else:
     total = 0
-timestart = time.time()
-irra = 0
-nhietdo = 0
-status = True
 
 with open("deviceConfig.conf", "r") as config_file:
     config = json.load(config_file)
 
-def send_orther(a,b):
-    time.sleep(0.1)
+def queue_data():
+    global data_queue
     client = mqtt.Client()
     client.username_pw_set('iot2022', 'iot2022')
+    while True:
+        if data_queue:
+            try:
+                data_item = data_queue.pop(0)
+                client.connect("core.ziot.vn", 5000) 
+                for topic, data in data_item.items():
+                    client.publish(topic, json.dumps(data))
+                    time.sleep(0.2)
+                    print(data)
+                print('Data is sent in queue :)))))))')
+
+            except:
+                data_queue.insert(0, data_item)
+                time.sleep(10)
+                
+        else:
+            time.sleep(10)
+
+def send_orther(a,b):
+    global data_queue
+    client = mqtt.Client()
+    client.username_pw_set('iot2022', 'iot2022')
+    mqtt_topic = str(a) + '/reportData'
+    data = json.dumps(b)
+ 
     try:
         client.connect("core.ziot.vn", 5000)
-        data = json.dumps(b)
-        mqtt_topic = str(a) + '/reportData'
         client.publish(mqtt_topic, data)
-        return
     except:
-        print("MQTT connect failed!!!   Try to connect with MQTT server...")
+        print("MQTT lost connection ... Data will be sent later")
+        data_lost = {mqtt_topic : b}
+        data_queue.append(data_lost)        
+        print(data_lost)
 
-        
 def reset_total():
-    timereset = (time.time() // 86400 + 1) * 86400
+    timereset = ((time.time()) // 86400 + 1) * 86400 - 7 * 3600
     global total
     while True:
         if time.time() >= timereset:
@@ -49,131 +76,66 @@ def reset_total():
             with open('backup.pickle', 'wb') as f:
                 pickle.dump(total, f)
             timereset = timereset + 86400
-        time.sleep(1)
+        time.sleep(3600)
 
-def restart():
+def restart(time4):
     time.sleep(0.1)
-    threading.Thread(target=read_data).start()  
+    threading.Thread(target=read_data, args= (time4,)).start()  
 
-
-def mqtt_reconnect():
-    time.sleep(0.1)
-    threading.Thread(target=send_data).start()    
-
-
-def read_data():
+def read_data(time3):
+    global status ,total,timestart, irra, nhietdo
     for device_id, device_info in config.get("modbustcp", {}).items():
         if device_info['deviceType'] == "sensor":
             ip = device_info['ip']
             unitID =  device_info['unitID']
-    global total,timestart, irra, nhietdo , status 
     client1 = ModbusClient(ip, port= 502, timeout=1)
 
     if client1.connect():
-            print("connected device")
-            while True:
-                if int(time.time()) >= int(timeset):
-
-                    read_orther()
-                    break                
-                try:
-                    client1.connect()  
-                    read1 = client1.read_input_registers(address=2, count=5, unit=unitID)
-                    nhietdo =  round(int(read1.registers[4])/100,1)
-                    a = str(format(read1.registers[0], '016b')) + str(format(read1.registers[1], '016b'))
-                    if a[0] == '1':
-                        irra = 0
-                    else:
-                        irra = int(a,2)/100
-                    
-                    time.sleep(0.2)
-                    total = total + (irra*(time.time()-timestart))/3600
-                    timestart = time.time()
-                    status = 1
-                    with open('backup.pickle', 'wb') as f:
-                        pickle.dump(total, f)
-                    timestart = time.time()
-                    # print(total)
-                    client1.close()
-                except:
-                    print("Device lost connection")
-                    status = 0
-                    restart()     
-                    break              
-    
-    else:
-        print("Connect failed")
-        status = 0
-        time.sleep(5)
-        print("Connect to the device failed!!!   Try to connect to the device...")
-        restart()
-
-def send_data(mm):
-    time.sleep(0.1)
-    mqtt_broker = "core.ziot.vn"
-    mqtt_port = 5000
-    mqtt_topic = "DEVICE/DO000000/PO000007/SI000008/PL000011/DE000139/reportData"
-    client = mqtt.Client()
-    client.username_pw_set('iot2022', 'iot2022')
-    try:
-        client.connect(mqtt_broker, mqtt_port)
-        print("MQTT connected server.")
-        client.disconnect()
-        sts = 1
-    except:
-        sts = 0
-    if sts == 1:
-
-        while True:
-
-            try:   
-
-                delaysec = 60
-                a = delaysec * (time.time() // delaysec) + delaysec            
-                time.sleep(a - time.time() )
-                timeStamp = datetime.datetime.fromtimestamp(a)
-
-                if  status == 1:
-                    irr = irra
-                    client.connect(mqtt_broker, mqtt_port)
-                    DATA = {"type": "smp3", "data": [{"totalIrradce": round(irr)}, {"dailyIrradtn": round(total/1000,3)}, {"ambientTemp": nhietdo}], "timeStamp": str(timeStamp)}
-                    data = json.dumps(DATA)
-                    client.publish(mqtt_topic, data)
-                    time.sleep(0.1)
-                    print(data,'\n')
-                    client.disconnect()                  
-                elif status == 0 :
-                    print("Device lost connection \nMQTT temporarily does not send data")
-                    mqtt_reconnect()    
-                    break
-                if int(time.time()) >= int(mm):
-                    break
-
-
+        print('Connected',ip,'502',unitID )
+        while time.time() < time3:           
+            try:  
+                read1 = client1.read_input_registers(address=2, count=5, unit=unitID)
+                nhietdo =  round(int(read1.registers[4])/100,1)
+                a = str(format(read1.registers[0], '016b')) + str(format(read1.registers[1], '016b'))
+                if a[0] == '1':
+                    irra = 0
+                irra = int(a,2)/100
+                # time.sleep(0.2)
+                total += (irra*(time.time()-timestart))/3600
+                timestart = time.time()
+                status = True
+                with open('backup.pickle', 'wb') as f:
+                    pickle.dump(total, f)
+                
             except:
-                print("MQTT lost connect")
-                time.sleep(5)
-                print("Try to connect with MQTT server...")
-                mqtt_reconnect()
-                break
-    
-    else:
-        time.sleep(5)
-        print("MQTT connect failed!!!   Try to connect with MQTT server...")
-        mqtt_reconnect() 
+                print("The device is lost connected to conversion")
+                status = False
+                restart(time3)   
+                return
 
-        
+
+        status = False      
+        read_orther() 
+        return  
+    else:
+        time.sleep(1)
+        print("Connect to the device failed!!!   Try to connect to the device...")
+        if time.time() >= time3:
+            read_orther() 
+            return
+        restart(time3)
+        return      
 
 def read_orther():
-    global timeset, data_package
+    global data_package, timeset
     with open("deviceConfig.conf", "r") as config_file:
         config = json.load(config_file)    
     # while True:
         for device_id, device_info in config.get("modbustcp", {}).items():
 
             client = ModbusTcpClient(device_info['ip'], port=device_info['port'], timeout=1)
-            # if device_info['deviceType'] == 'sensor':
-            #     continue
+            if device_info['deviceType'] == 'sensor':
+                continue
             try:
             
                 client.connect()
@@ -197,6 +159,7 @@ def read_orther():
                         value = round(struct.unpack('f', struct.pack('I', combined_data))[0]*(10 ** register['PF']),register['fractionDigit'])
 
                     elif register['dataType'] == 'int16':
+
                         value = round((struct.unpack('>h', struct.pack('>H', data[0]))[0])*(10 ** register['PF']),register['fractionDigit'])
 
                     elif register['dataType'] == 'int32':
@@ -209,7 +172,7 @@ def read_orther():
 
                     # time.sleep(0.1)
                 client.close()
-                print('Connected',device_info['ip'],device_info['port'],device_info['unitID'] )
+                print('Connected',device_info['ip'],device_info['port'],device_info['unitID'])
                 DATA_1 = {"type": device_info['deviceType'],"data" : data_fomat , "timeStamp": str(datetime.datetime.fromtimestamp((time.time())))}
                 print(DATA_1)
                 threading.Thread(target=send_orther, args= (device_id,DATA_1,)).start()
@@ -220,12 +183,26 @@ def read_orther():
                 data_package = {}  
             time.sleep(0.1)  
     timeset = device_info['scanningCycleInSecond'] * (time.time() // device_info['scanningCycleInSecond'] + 1) 
+    # timeset = (time.time() // 30 + 1) * 30
     run_main(timeset)
+    return
 
+def call_data():
+    time.sleep(1)
+    mqtt_topic = "DEVICE/DO000000/PO000007/SI000008/PL000011/DE000139"
+    while True:
+        while status:
+            time.sleep(delaysec * (time.time() // delaysec + 1)  - time.time())
+            timeStamp = datetime.datetime.fromtimestamp(delaysec * (time.time() // delaysec) )
+            data_call = {"type": "smp3", "data": [{"totalIrradce": round(irra)}, {"dailyIrradtn": round(total/1000,3)}, {"ambientTemp": nhietdo}], "timeStamp": str(timeStamp)}
+            print(data_call)
+            threading.Thread(target=send_orther, args= (mqtt_topic,data_call,)).start()
+        time.sleep(1)
 
-def run_main(m):  
-    threading.Thread(target=reset_total).start()    
-    threading.Thread(target=send_data, args= (m,)).start()
-    threading.Thread(target=read_data).start()
-
-read_orther()
+def run_main(time1):    
+    threading.Thread(target=read_data, args= (time1,)).start()
+if __name__ == "__main__":
+    threading.Thread(target=queue_data).start()
+    threading.Thread(target=reset_total).start()  
+    threading.Thread(target=call_data).start()
+    read_orther()
